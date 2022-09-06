d8a00b57f66d2b2dd8f49ce9ad1e5b546844cd74
Samuel Arguello <80545434+Sarguello12@users.noreply.github.com>
2022-09-06 11:09:16 -0500

    feat: added navigation bar and layout component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,18 @@
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-	return <h1>Quote Details</h1>;
+	const params = useParams();
+
+	return (
+		<section>
+			<h1>Quote Details</h1>
+			<p>{params.quoteId}</p>
+			<Route path={`/quotes/${params.quoteId}/comments`}>
+				<Comments />
+			</Route>
+		</section>
+	);
 };
 
 export default QuoteDetail;
