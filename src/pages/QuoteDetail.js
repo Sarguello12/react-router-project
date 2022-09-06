4c81532ac24bcac94cd70bc289b7828fc4b00a5e
Samuel Arguello <80545434+Sarguello12@users.noreply.github.com>
2022-09-06 13:02:58 -0500

    fix: refactored solution to use router hooks useHistory, useLocation, useParam, useRouteMatch, etc...

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
@@ -8,6 +8,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+	const match = useRouteMatch();
 	const params = useParams();
 
 	const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -19,7 +20,14 @@ const QuoteDetail = () => {
 	return (
 		<section>
 			<HighlightedQuote text={quote.text} author={quote.author} />
-			<Route path={`/quotes/${params.quoteId}/comments`}>
+			<Route path={match.path} exact>
+				<div className="centered">
+					<Link className="btm--flat" to={`${match.url}/comments`}>
+						Load Comments
+					</Link>
+				</div>
+			</Route>
+			<Route path={`${match.path}/comments`}>
 				<Comments />
 			</Route>
 		</section>
@@ -27,3 +35,5 @@ const QuoteDetail = () => {
 };
 
 export default QuoteDetail;
+
+// useRouteMatch: The useRouteMatch hook attempts to match the current URL in the same way that a <Route> would. It’s mostly useful for getting access to the match data without actually rendering a <Route>.
